fa20d6c39ef32ab6d92e7ad2d624258941245bc3
Ashish Agarwal <agarwal1975@gmail.com>
2014-01-14 18:19:53 -0500

    Lwt_internal_pervasives: define Future_lwt module
    A Lwt implementation of the Future.S interface.

--- src/lib_lwt/biocaml_lwt_internal_pervasives.mli
+++ src/lib_lwt/biocaml_lwt_internal_pervasives.mli
@@ -1,2 +1,7 @@
 include module type of Biocaml_internal_pervasives
 include module type of Lwt
+
+module Future_lwt : Future.S
+  with type 'a Deferred.t = 'a Lwt.t
+  and type 'a Pipe.Reader.t = 'a Lwt_stream.t
+  and type Reader.t = Lwt_io.input_channel
