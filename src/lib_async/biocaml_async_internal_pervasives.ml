c8989ab545f445976dcba318c2c4cd9c69f21649
Ashish Agarwal <agarwal1975@gmail.com>
2014-01-15 15:35:56 -0500

    define Future_async module

--- src/lib_async/biocaml_async_internal_pervasives.ml
+++ src/lib_async/biocaml_async_internal_pervasives.ml
@@ -1,2 +1,26 @@
 include Biocaml_internal_pervasives
 open Async.Std
+
+module Future_async = struct
+  module Deferred = Deferred
+
+  let return = return
+  let (>>=) = (>>=)
+  let (>>|) = (>>|)
+  let (>>=?) = (>>=?)
+  let (>>|?) = (>>|?)
+  let fail = raise
+  let raise = `Use_fail_instead
+
+  module Pipe = struct
+    include Pipe
+    let read r = read r
+    let fold r ~init ~f = fold r ~init ~f
+  end
+
+  module Reader = struct
+    include Reader
+    let with_file ?buf_len file ~f = with_file ?buf_len file ~f
+  end
+
+end
--- src/lib_async/biocaml_async_internal_pervasives.mli
+++ src/lib_async/biocaml_async_internal_pervasives.mli
@@ -1,2 +1,7 @@
 include module type of Biocaml_internal_pervasives
 open Async.Std
+
+module Future_async : Future.S
+  with type 'a Deferred.t = 'a Deferred.t
+  and type 'a Pipe.Reader.t = 'a Pipe.Reader.t
+  and type Reader.t = Reader.t
